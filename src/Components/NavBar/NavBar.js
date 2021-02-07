f9bf354b2c2627e1a911c76e47ae7188907a9348
Fede Lemaire <fede.lemaire@gmail.com>
2021-02-07 02:48:38 -0300

    first commit

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import './navbar.css'
+
+export default function NavBar({ type, setTipo }) {
+
+    const [vip, setVip] = useState("Inicia sesion");
+    const [user, setUser] = useState("");
+
+    useEffect(() => {
+        const nombre = setTimeout(() => {
+            setUser("Bienvenid@")
+        }, 2000);
+        return () => clearTimeout(nombre);
+    }, []);
+
+    const handleVip = () => {
+        if (type === "inicia") {
+            setTipo("cerrar");
+            setVip("cerra sesion");
+        } else {
+            setTipo("inicia");
+            setVip("Inicia sesion");
+
+        }
+    };
+
+    return (
+        <header>
+            <div className={`navStyle-${type}`}>
+                <div className='ul'>
+                    <NavLink to='/' className='links-nav'><p className='nav-link-nav'>Home</p></NavLink>
+                    <NavLink to='/categories' className='links-nav'><p className='nav-link'>categorias</p></NavLink>
+                </div>
+                <div>
+
+                    <button onClick={handleVip} className="buttonVip">
+                        <p className={`to-${type}`}>{vip}</p>
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
+}
+
